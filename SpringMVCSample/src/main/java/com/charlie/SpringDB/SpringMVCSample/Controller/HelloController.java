a359416911d1c3077d6ebd200fdb79eafa99cfa2
charlesxieyupeng <charlesxieyupeng@gmail.com>
2013-06-01 17:23:41 -0400

    6/1

---
+++ SpringMVCSample/src/main/java/com/charlie/SpringDB/SpringMVCSample/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.charlie.SpringDB.SpringMVCSample.Controller;
+
+public class HelloController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
